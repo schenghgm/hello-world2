7bef4856cf0d6730ebac2648abd38dc26210a3bf
Shao <shaoshao.cheng@rule14.com>
2024-04-02 14:43:24 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `hello world`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
